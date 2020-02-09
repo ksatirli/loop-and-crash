2533525dc8cef5486c0ac5369c6bc0672eef2de6
Kerim Satirli <ksatirli@users.noreply.github.com>
2020-02-09 12:38:10 +0100

    Adds Standard and expands tests (#6)
    * linting with Standard.js
    
    * updates tests

--- test/routes.js
+++ test/routes.js
@@ -1,42 +1,45 @@
-'use strict';
+'use strict'
 
-//During the test the env variable is set to test
-process.env.NODE_ENV = 'test';
+// During the test the env variable is set to test
+process.env.NODE_ENV = 'test'
 
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var server = require('../index');
-var should = chai.should(); // jshint unused:false
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const server = require('../index')
+const should = chai.should()
 
-chai.use(chaiHttp);
+chai.use(chaiHttp)
 
 describe('Routes', () => {
-
   describe('/', () => {
     it('GET / should be HTML', (done) => {
       chai.request(server)
-      .get('/')
-      .end((err, res) => {
-        res.should.be.html;
-        res.should.have.status(200);
+        .get('/')
+        .end((err, res) => {
+          should.not.exist(err)
+          should.exist(res)
+          res.should.be.html // eslint-disable-line
+          res.should.have.status(200)
 
-        done();
-      });
-    });
-  });
+          done()
+        })
+    })
+  })
 
   describe('/style.css', () => {
     it('GET /style.css should be CSS', (done) => {
       chai.request(server)
-      .get('/style.css')
-      .end((err, res) => {
-        res.should.have.header('content-type', 'text/css; charset=utf-8');
-        res.should.have.status(200);
+        .get('/style.css')
+        .end((err, res) => {
+          should.not.exist(err)
+          should.exist(res)
+          res.should.have.header('content-type', 'text/css; charset=utf-8')
+          res.should.have.status(200)
 
-        done();
-      });
-    });
-  });
+          done()
+        })
+    })
+  })
 
   // describe('GET /crash', () => {
   //   it('should respond with HTTP Status Code 503', (done) => {
@@ -56,103 +59,145 @@ describe('Routes', () => {
   describe('/show-get-params', () => {
     it('GET /show-get-params should have URL Query parameters', (done) => {
       chai.request(server)
-      .get('/show-get-params')
-      .query({foo: 'bar'})
-      .end((err, res) => {
-        res.should.have.header('content-type', 'application/json; charset=utf-8');
-        res.should.have.status(200);
+        .get('/show-get-params')
+        .query({ foo: 'bar' })
+        .end((err, res) => {
+          should.not.exist(err)
+          should.exist(res)
+          res.should.have.header('content-type', 'application/json; charset=utf-8')
+          res.should.have.status(200)
 
-        done();
-      });
-    });
-  });
+          done()
+        })
+    })
+  })
 
   describe('/status-code', () => {
     it('GET /status-code should be HTML', (done) => {
       chai.request(server)
-      .get('/status-code')
-      .end((err, res) => {
-        res.should.be.html;
-        res.should.have.status(200);
+        .get('/status-code')
+        .end((err, res) => {
+          should.not.exist(err)
+          should.exist(res)
+          res.should.be.html // eslint-disable-line
+          res.should.have.status(200)
 
-        done();
-      });
-    });
+          done()
+        })
+    })
 
     it('GET /status-code/200 should respond with HTTP Status Code 200', (done) => {
       chai.request(server)
-      .get('/status-code/200')
-      .end((err, res) => {
-        res.should.have.header('content-type', 'text/plain; charset=utf-8');
-        res.should.have.status(200);
+        .get('/status-code/200')
+        .end((err, res) => {
+          should.not.exist(err)
+          should.exist(res)
+          res.should.have.header('content-type', 'text/plain; charset=utf-8')
+          res.should.have.status(200)
 
-        done();
-      });
-    });
-  });
+          done()
+        })
+    })
+
+    it('GET /status-code/400 should respond with HTTP Status Code 500', (done) => {
+      chai.request(server)
+        .get('/status-code/500')
+        .end((err, res) => {
+          should.not.exist(err)
+          should.exist(res)
+          res.should.have.header('content-type', 'text/plain; charset=utf-8')
+          res.should.have.status(500)
+
+          done()
+        })
+    })
+
+    it('GET /status-code/500 should respond with HTTP Status Code 400', (done) => {
+      chai.request(server)
+        .get('/status-code/400')
+        .end((err, res) => {
+          should.not.exist(err)
+          should.exist(res)
+          res.should.have.header('content-type', 'text/plain; charset=utf-8')
+          res.should.have.status(400)
+
+          done()
+        })
+    })
+  })
 
   describe('/delay', () => {
     it('GET /delay should be HTML', (done) => {
       chai.request(server)
-      .get('/delay')
-      .end((err, res) => {
-        res.should.be.html;
-        res.should.have.status(200);
+        .get('/delay')
+        .end((err, res) => {
+          should.not.exist(err)
+          should.exist(res)
+          res.should.be.html // eslint-disable-line
+          res.should.have.status(200)
 
-        done();
-      });
-    });
+          done()
+        })
+    })
 
     it('GET /delay/1 should be plain-text', (done) => {
       chai.request(server)
-      .get('/delay/1')
-      .end((err, res) => {
-        res.should.have.header('content-type', 'text/plain; charset=utf-8');
-        res.should.have.status(200);
+        .get('/delay/1')
+        .end((err, res) => {
+          should.not.exist(err)
+          should.exist(res)
+          res.should.have.header('content-type', 'text/plain; charset=utf-8')
+          res.should.have.status(200)
 
-        done();
-      });
-    });
-  });
+          done()
+        })
+    })
+  })
 
-  // describe('/info', () => {
-  //   it('GET /info should be JSON', (done) => {
-  //     chai.request(server)
-  //     .get('/info')
-  //     .end((err, res) => {
-  //       res.should.have.header('content-type', 'application/json; charset=utf-8');
-  //       res.should.have.status(200);
-  //       res.req.res.body.should.exist;
-  //
-  //       done();
-  //     });
-  //   });
+  describe('/info', () => {
+    it('GET /info should be JSON', (done) => {
+      chai.request(server)
+        .get('/info')
+        .end((err, res) => {
+          should.not.exist(err)
+          should.exist(res)
+          res.should.have.header('content-type', 'application/json; charset=utf-8')
+          res.should.have.status(200)
+          res.body.should.exist // eslint-disable-line
 
-    // it('GET /info?interfaces=1 should include network interface(s) information', (done) => {
-    //   chai.request(server)
-    //   .get('/info')
-    //   .query({interfaces: 1})
-    //   .end((err, res) => {
-    //     res.should.have.header('content-type', 'application/json; charset=utf-8');
-    //     res.should.have.status(200);
-    //     res.req.res.body.network.should.exist;
-    //     res.req.res.body.network.interfaces.should.exist;
-    //
-    //     done();
-    //   });
-    // });
+          done()
+        })
+    })
 
-  //   it('GET /info?environment=1 should include environment information', (done) => {
-  //     chai.request(server)
-  //     .get('/info')
-  //     .query({environment: 1})
-  //     .end((err, res) => {
-  //       res.should.have.header('content-type', 'application/json; charset=utf-8');
-  //       res.should.have.status(200);
-  //       res.req.res.body.environment.should.exist;
-  //
-  //       done();
-  //     });
-  //   });
-  // });
-});
+    it('GET /info?interfaces=1 should include network interface(s) information', (done) => {
+      chai.request(server)
+        .get('/info')
+        .query({ interfaces: 1 })
+        .end((err, res) => {
+          should.not.exist(err)
+          should.exist(res)
+          res.should.have.header('content-type', 'application/json; charset=utf-8')
+          res.should.have.status(200)
+          res.body.network.should.exist // eslint-disable-line
+          res.body.network.interfaces.should.exist // eslint-disable-line
+
+          done()
+        })
+    })
+
+    it('GET /info?environment=1 should include environment information', (done) => {
+      chai.request(server)
+        .get('/info')
+        .query({ environment: 1 })
+        .end((err, res) => {
+          should.not.exist(err)
+          should.exist(res)
+          res.should.have.header('content-type', 'application/json; charset=utf-8')
+          res.should.have.status(200)
+          res.body.environment.should.exist // eslint-disable-line
+
+          done()
+        })
+    })
+  })
+})
